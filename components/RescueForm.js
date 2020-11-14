97b5239c88008e874aa843d156b47cdf5b2abae0
David Marquez <david.marquez@me.com>
2020-11-14 22:48:48 +0800

    Polish

--- components/RescueForm.js
+++ components/RescueForm.js
@@ -36,20 +36,20 @@ export default function RescueForm() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-200 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-lg">
-        <img
+        {/* <img
           className="mx-auto h-12 w-auto"
           src="https://tailwindui.com/img/logos/v1/workflow-mark-on-white.svg"
           alt="Workflow"
-        />
+        /> */}
         <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
           Fill up the Rescue Form
         </h2>
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-lg">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="bg-white py-8 px-4 shadow-xl sm:rounded-lg sm:px-10">
           <Formik
             initialValues={initialValues}
             onSubmit={async (values, { setSubmitting }) => {
@@ -205,7 +205,7 @@ export default function RescueForm() {
                       disabled={isSubmitting}
                       className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
                     >
-                      Sign in
+                      Submit
                     </button>
                   </span>
                 </div>
